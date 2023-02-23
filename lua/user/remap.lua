776887bd9bf0d759dc4ad8795eb64bbfa1627c0e
Dulguun Otgon <dulguun0225@gmail.com>
2023-02-23 16:51:36 +0800

    idk

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,5 +1,6 @@
 local set = vim.keymap.set
 local cmd = vim.cmd
+local opts = { noremap = true, silent = true }
 vim.g.mapleader = " "
 --
 -- NvimTree
@@ -8,30 +9,29 @@ set("n", "<C-n>", cmd.NvimTreeToggle)
 
 -- BarBar
 -- Move to previous/next
-set('n', '<A-,>', '<Cmd>BufferPrevious<CR>')
-set('n', '<A-.>', '<Cmd>BufferNext<CR>')
+set('n', '<A-,>', '<Cmd>BufferPrevious<CR>',opts)
+set('n', '<A-.>', '<Cmd>BufferNext<CR>',opts)
 -- Re-order to previous/next
-set('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>')
-set('n', '<A->>', '<Cmd>BufferMoveNext<CR>')
+set('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>',opts)
+set('n', '<A->>', '<Cmd>BufferMoveNext<CR>',opts)
 -- Goto buffer in position...
-set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>')
-set('n', '<A-2>', '<Cmd>BufferGoto 2<CR>')
-set('n', '<A-3>', '<Cmd>BufferGoto 3<CR>')
-set('n', '<A-4>', '<Cmd>BufferGoto 4<CR>')
-set('n', '<A-5>', '<Cmd>BufferGoto 5<CR>')
-set('n', '<A-6>', '<Cmd>BufferGoto 6<CR>')
-set('n', '<A-7>', '<Cmd>BufferGoto 7<CR>')
-set('n', '<A-8>', '<Cmd>BufferGoto 8<CR>')
-set('n', '<A-9>', '<Cmd>BufferGoto 9<CR>')
-set('n', '<A-0>', '<Cmd>BufferLast<CR>')
+set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>',opts)
+set('n', '<A-2>', '<Cmd>BufferGoto 2<CR>',opts)
+set('n', '<A-3>', '<Cmd>BufferGoto 3<CR>',opts)
+set('n', '<A-4>', '<Cmd>BufferGoto 4<CR>',opts)
+set('n', '<A-5>', '<Cmd>BufferGoto 5<CR>',opts)
+set('n', '<A-6>', '<Cmd>BufferGoto 6<CR>',opts)
+set('n', '<A-7>', '<Cmd>BufferGoto 7<CR>',opts)
+set('n', '<A-8>', '<Cmd>BufferGoto 8<CR>',opts)
+set('n', '<A-9>', '<Cmd>BufferGoto 9<CR>',opts)
+set('n', '<A-0>', '<Cmd>BufferLast<CR>',opts)
 -- Pin/unpin buffer
-set('n', '<A-p>', '<Cmd>BufferPin<CR>')
+set('n', '<A-p>', '<Cmd>BufferPin<CR>',opts)
 -- Close buffer
-set('n', '<A-c>', '<Cmd>BufferClose<CR>')
-set('n', '<A-S-c>', '<Cmd>BufferClose!<CR>')
+set('n', '<A-c>', '<Cmd>BufferClose<CR>',opts)
+set('n', '<A-S-c>', '<Cmd>BufferClose!<CR>',opts)
 -- Save buffer
 set('n', '<A-s>', cmd.w)
-set('i', '<A-s>', cmd.w)
 set('n', '<A-n>', cmd.enew)
 set('n', '<A-f>', vim.lsp.buf.format)
 -- Wipeout buffer
@@ -43,39 +43,34 @@ set('n', '<A-f>', vim.lsp.buf.format)
 --                 :BufferCloseBuffersLeft
 --                 :BufferCloseBuffersRight
 -- Magic buffer-picking mode
-set('n', '<C-p>', '<Cmd>BufferPick<CR>')
+set('n', '<C-p>', '<Cmd>BufferPick<CR>',opts)
 -- Sort automatically by...
-set('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>')
-set('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>')
-set('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>')
-set('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>')
+set('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>',opts)
+set('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>',opts)
+set('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>',opts)
+set('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>',opts)
 
-
-
-
-
-
-set("i", "kj", "<Esc>")
-
+set("i", "kj", "<Esc>", opts)
+    
 -- Telescope
-local tsb = require('telescope.builtin')
-set('n', '<leader>fff', tsb.find_files, {})
-set('n', '<leader>fg', tsb.live_grep, {})
-set('n', '<leader>fb', tsb.buffers, {})
-set('n', '<leader>fh', tsb.help_tags, {})
-set('n', '<leader>ff', tsb.git_files, {})
+local tsb = require('telescope.builtin',opts)
+set('n', '<leader>fa', tsb.find_files, opts)
+set('n', '<leader>fg', tsb.live_grep, opts)
+set('n', '<leader>fb', tsb.buffers, opts)
+set('n', '<leader>fh', tsb.help_tags, opts)
+set('n', '<leader>ff', tsb.git_files, opts)
 
 -- Fugivitve
 set('n', '<leader>git', cmd.Git)
 
 -- Custom
-set("n", "<C-h>", "<C-w>h", {})
-set("n", "<C-j>", "<C-w>j", {})
-set("n", "<C-k>", "<C-w>k", {})
-set("n", "<C-l>", "<C-w>l", {})
+set("n", "<C-h>", "<C-w>h", opts)
+set("n", "<C-j>", "<C-w>j", opts)
+set("n", "<C-k>", "<C-w>k", opts)
+set("n", "<C-l>", "<C-w>l", opts)
 
 
-set("n", "<C-Right>", "<C-w>>", {})
-set("n", "<C-Left>", "<C-w><", {})
-set("n", "<C-Up>", "<C-w>+", {})
-set("n", "<C-Down>", "<C-w>-", {})
+set("n", "<C-Right>", "<C-w>>", opts)
+set("n", "<C-Left>", "<C-w><", opts)
+set("n", "<C-Up>", "<C-w>+", opts)
+set("n", "<C-Down>", "<C-w>-", opts)
